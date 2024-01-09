810f76118678773ab26101eb74aa4b364c00c885
Thurman Thoma <thurman.thoma@gmail.com>
2024-01-08 22:40:33 -0600

    Test connection

--- html/index.php
+++ html/index.php
@@ -27,7 +27,10 @@
             // Create Connection
             $conn = new mysqli($servername, $username, $password, $database);
 
-            echo $conn;
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
         ?>
     </section>
 
