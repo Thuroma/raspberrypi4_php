5357747578730a4449a2a1c66cd60e0b69188ae8
Thurman Thoma <thurman.thoma@gmail.com>
2024-01-09 21:52:49 -0600

    t

--- src/Model/RecipeManager.php
+++ src/Model/RecipeManager.php
@@ -43,18 +43,13 @@ class RecipeManager {
                     Recipe.recipe_id,
                     Recipe.name,
                     Recipe.description,
-                    GROUP_CONCAT(Ingredient.name SEPARATOR ', ') AS ingredient_list,
-                    -- GROUP_CONCAT(Instruction.instruction SEPARATOR ', ') AS instruction_list
+                    GROUP_CONCAT(Ingredient.name SEPARATOR ', ') AS ingredient_list
                 FROM
                     Recipe
                 LEFT JOIN
                     RecipeIngredient ON Recipe.recipe_id = RecipeIngredient.recipe_id
                 LEFT JOIN
                     Ingredient ON RecipeIngredient.ingredient_id = Ingredient.ingredient_id
-                -- LEFT JOIN
-                --     RecipeInstruction ON Recipe.recipe_id = RecipeInstruction.recipe_id
-                -- LEFT JOIN
-                --     Instruction ON RecipeInstruction.instruction_id = Instruction.instruction_id
                 GROUP BY
                     Recipe.recipe_id, Recipe.name, Recipe.description
                 ORDER BY
@@ -73,8 +68,7 @@ class RecipeManager {
                 'id' => $row['recipe_id'],
                 'name' => $row['name'],
                 'description' => $row['description'],
-                'ingredients' => $row['ingredient_list'],
-                // 'instructions' => $row['instruction_list']
+                'ingredients' => $row['ingredient_list']
             ];
         }
 
